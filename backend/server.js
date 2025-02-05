3d3207918879a181eb3cface4556f5a62b865bf9
Preston Gilmore <prestongilmore@Prestons-MacBook-Pro.local>
2025-02-04 19:13:52 -0700

    added mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,24 @@
 require('dotenv').config()
 
 const express = require('express')
+const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app = express();
 
-// routes
-app.get('/', (req, res) => {
-    res.json({mssg: 'welcome to this page'})
+// middleware
+app.use(express.json())
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
 })
+
+// routes
+// app.get('/', (req, res) => {
+//     res.json({mssg: 'welcome to this page'})
+// })
+
+app.use('/api/workouts', workoutRoutes) // requiring api path workouts
 
 // listen for requests 
 app.listen(process.env.PORT, () => {
